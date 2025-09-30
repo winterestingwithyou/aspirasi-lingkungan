73c1fc352a83a58eacdba73e4b36a538bfd412b5
Winterest | M. Adam Yudistira <yudistiraadam3@gmail.com>
2025-09-30 17:57:20 +0700

    refactor kode getAddress agar menggunakan axios

--- app/utils/getAddress.ts
+++ app/utils/getAddress.ts
@@ -1,10 +1,16 @@
+import axios from 'axios';
 import type { NominatimResponse } from '~/types';
 
-export async function getAddress(lat: number, lon: number): Promise<string> {
+async function getAddress(lat: number, lon: number): Promise<string> {
   try {
-    const res = await fetch(
-      `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`,
+    const res = await axios.get<NominatimResponse>(
+      'https://nominatim.openstreetmap.org/reverse',
       {
+        params: {
+          lat,
+          lon,
+          format: 'json',
+        },
         headers: {
           Accept: 'application/json',
           'User-Agent': 'MyReactApp/1.0 (myemail@example.com)', // wajib
@@ -12,12 +18,11 @@ export async function getAddress(lat: number, lon: number): Promise<string> {
       },
     );
 
-    if (!res.ok) throw new Error('Gagal request ke Nominatim');
-
-    const data: NominatimResponse = await res.json();
-    return data.display_name || 'Alamat tidak ditemukan';
+    return res.data.display_name || 'Alamat tidak ditemukan';
   } catch (err) {
     console.error(err);
     return 'Terjadi kesalahan saat mengambil alamat';
   }
 }
+
+export { getAddress };
