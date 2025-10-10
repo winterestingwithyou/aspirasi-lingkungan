b640a226c48630e5e8e13f6620d6121bd7b78d57
Winterest | M. Adam Yudistira <yudistiraadam3@gmail.com>
2025-10-10 10:13:31 +0700

    fix bug dimana css lambat di load

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,13 +1,12 @@
 import { startTransition, StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { HydratedRouter } from 'react-router/dom';
+import('bootstrap-icons/font/bootstrap-icons.css');
+import('./app.css');
+import('./bootstrap.css');
+import('leaflet/dist/leaflet.css');
 
 startTransition(async () => {
-  await import('bootstrap-icons/font/bootstrap-icons.css');
-  await import('./app.css');
-  await import('./bootstrap.css');
-  await import('leaflet/dist/leaflet.css');
-
   hydrateRoot(
     document,
     <StrictMode>
